28a27a5da3211ad9db8d3999b41e12037ba31e70
Diego Martins Sanchez <diego.sanchez@atitus.local>
2025-10-09 19:28:06 -0300

    discovery e currency atualizados

--- greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
+++ greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
@@ -5,16 +5,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.atitus.greeting_service.configs.GreetingConfig;
-import br.edu.atitus.greeting_service.dto.GreetingRequest;
+import br.edu.atitus.greeting_service.dtos.GreetingDTO;
 
 @RestController
 @RequestMapping("greeting")
 public class GreetingController {
+	
+	//@Value("${greeting-service.greeting}")
+	//private String greeting;
+	//@Value("${greeting-service.default-name}")
+	//private String defaultName;
 	
 	private final GreetingConfig config;
 	
@@ -22,33 +27,48 @@ public class GreetingController {
 		super();
 		this.config = config;
 	}
-
-	// Suporta: /greeting e /greeting?name=Chewbacca
-	@GetMapping
-	public ResponseEntity<String> greet(@RequestParam(required = false) String name) {
+	
+	
+	@GetMapping({"", "/{namePath}"})
+	public ResponseEntity<String> greet(
+			@RequestParam(required = false) String name,
+			@PathVariable(required = false) String namePath){
 		String greetingReturn = config.getGreeting();
-		String nameReturn = name != null ? name : config.getDefaultName();
+		String nameReturn = name != null 
+				? name 
+				: namePath != null ? namePath : config.getDefaultName();
 		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
 		
 		return ResponseEntity.ok(textReturn);
 	}
-
-	// Suporta: /greeting/Chewbacca
-	@GetMapping("/{name}")
-	public ResponseEntity<String> greetWithPathVariable(@PathVariable String name) {
-		String greetingReturn = config.getGreeting();
-		String textReturn = String.format("%s, %s!!!", greetingReturn, name);
-		
-		return ResponseEntity.ok(textReturn);
-	}
-
-	// Suporta: POST /greeting com JSON { "name": "Leia" }
+	
 	@PostMapping
-	public ResponseEntity<String> greetPost(@RequestBody GreetingRequest request) {
+	public ResponseEntity<String> greetPost(
+			@RequestBody GreetingDTO dto){
 		String greetingReturn = config.getGreeting();
-		String nameReturn = request.getName() != null ? request.getName() : config.getDefaultName();
+		String nameReturn = dto.name(); 
 		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
 		
 		return ResponseEntity.ok(textReturn);
 	}
+
+	
+//	@GetMapping("/{namePath}")
+//	public ResponseEntity<String> greetPaht(
+//			@PathVariable String namePath){
+//		return greet(namePath);
+//	}
+//
+//	@GetMapping
+//	public ResponseEntity<String> greet(
+//			@RequestParam(required = false) String name){
+//		String greetingReturn = config.getGreeting();
+//		String nameReturn = name != null ? name : config.getDefaultName();
+//		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
+//		
+//		return ResponseEntity.ok(textReturn);
+//	}
+
+	
+	
 }
